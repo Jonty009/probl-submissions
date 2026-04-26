9e043265df55f3bf263b42e860024c8660293e12
Arunava Kundu <36809802+Jonty009@users.noreply.github.com>
2026-04-27 01:17:47 +0530

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.py
@@ -0,0 +1,32 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)
+        # print('DEBUG: nums sorted:', nums)
+        
+
+        res = []
+
+        for i, a in enumerate(nums):
+            # print(f"DEBUG: i={i}, a={a}")
+
+            if i>0 and a==nums[i-1]:
+                continue
+
+            l = i+1
+            r = len(nums) - 1
+            while l<r:
+                sum = a + nums[l] + nums[r]
+                # print(f"DEBUG: i={i}, l={l}, r={r}, sum={sum}")
+                if sum<0:
+                    l+=1
+                elif sum>0:
+                    r -= 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    # print(f"DEBUG: Found triplet: [{a}, {nums[l]}, {nums[r]}]")
+                    l+=1
+                    r -= 1
+                    while nums[l] == nums[l-1] and l<r:
+                        # print(f"DEBUG: Skipping duplicate at l={l}, nums[l]={nums[l]}")
+                        l+=1
+        return res
